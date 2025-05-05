df450ec73bb663bf6b3d5402916f086d6feaf196
ioanaoprea11 <ioanamarcela58@gmail.com>
2025-05-05 10:08:38 +0300

    Flyweight verification

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import flyweight.models.Caracter;
+import flyweight.models.ETipFont;
+import flyweight.models.StilCaracter;
+import flyweight.models.StilCaracterFactory;
+
 public class Main {
+    //Au fost create 3 caractere, două dintre ele având același stil
+    //Stilul celui de al doilea caracter nu a fost creat din nou
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StilCaracter stil1 = StilCaracterFactory.getStil("Arial", 12, ETipFont.BOLD, "negru");
+        stil1.afiseaza(new Caracter('A', 0));
+
+        StilCaracter stil2 = StilCaracterFactory.getStil("Arial", 12, ETipFont.BOLD, "negru");
+        stil2.afiseaza(new Caracter('B', 1));
+
+        StilCaracter stil3 = StilCaracterFactory.getStil("Times New Roman", 14, ETipFont.ITALIC, "rosu");
+        stil3.afiseaza(new Caracter('C', 2));
+
+        System.out.println("\nTotal stiluri create (unice): " + StilCaracterFactory.getNrStiluriCreate());
+        System.out.println("Total cereri stiluri (inclusiv duplicate): " + StilCaracterFactory.getNrStiluriCerute());
     }
 }
